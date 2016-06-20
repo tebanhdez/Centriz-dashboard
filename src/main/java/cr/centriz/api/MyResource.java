023b346ecf40183e05b1c73b6ff8c11876840ff2
Esteban Hernandez <Esteban.Hernandez@flatironssolutions.com>
2016-06-20 11:05:54 -0600

    Added User - UserRoles models

--- src/main/java/cr/centriz/api/MyResource.java
+++ src/main/java/cr/centriz/api/MyResource.java
@@ -1,13 +1,13 @@
 package cr.centriz.api;
 
-import cr.centriz.utils.HibernateUtil;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import org.hibernate.Session;
+
+import cr.centriz.utils.HibernateUtil;
 
 /**
  * Root resource (exposed at "myresource" path)
@@ -18,7 +18,6 @@ public class MyResource {
     /**
      * Method handling HTTP GET requests. The returned object will be sent
      * to the client as "text/plain" media type.
-     *
      * @return String that will be returned as a text/plain response.
      */
     @GET
--- src/main/java/cr/centriz/entities/User.java
+++ src/main/java/cr/centriz/entities/User.java
@@ -1,11 +1,17 @@
 package cr.centriz.entities;
 
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @Entity
@@ -56,6 +62,29 @@ public class User {
     }
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "creation_date", updatable = false)
+    private Date creationDate;
+    
+    public Date getCreationDate() {
+        return creationDate;
+    }
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    @ManyToOne(optional=false)
+    @JoinColumn(name="user_role_id", nullable=false)
+    private UserRole role;
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public void setRole(UserRole role) {
+        this.role = role;
     }
 
     @Override
