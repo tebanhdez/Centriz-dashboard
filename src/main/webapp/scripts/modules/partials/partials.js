e73b3cb0863c05e827328a6cc791360bd251843f
Luis Serrano <lserrano@pernix-solutions.com>
2016-06-29 13:46:35 -0600

    separate directives in dashboard interface

---
+++ src/main/webapp/scripts/modules/partials/partials.js
@@ -0,0 +1,32 @@
+'use strict';
+
+angular.module('partials')
+
+.directive('mainNavbar', function() {
+  return {
+    restrict: 'E',
+    templateUrl: "views/partials/navbar.html"
+  }
+})
+
+.directive('sidebarLeft', function() {
+  return {
+    restrict: 'E',
+    templateUrl: "views/partials/sidebar-left.html"
+  }
+})
+
+.directive('sidebarRigth', function() {
+  return {
+    restrict: 'E',
+    templateUrl: "views/partials/sidebar-rigth.html"
+  }
+})
+
+.directive('workArea', function() {
+  return {
+    restrict: 'E',
+    templateUrl: "views/partials/work-area.html"
+  }
+});
+
