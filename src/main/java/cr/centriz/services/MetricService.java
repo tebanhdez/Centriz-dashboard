27c02f0f5aec047d99a6d0ea21972cb5cb597dee
Luis Serrano <lserrano@pernix-solutions.com>
2016-07-11 15:52:08 -0600

    remove hard code Json, add metrics to service

--- src/main/java/cr/centriz/services/MetricService.java
+++ src/main/java/cr/centriz/services/MetricService.java
@@ -42,9 +42,61 @@ public class MetricService {
         inventory.addLevel(renting);
         Level process = new Level("Procesos");
         process.addLevel(inventory);
-
         data.addLevel(process);
 
+        Metric metric2 = new Metric("Metric", sdf.parse("20160101"), sdf.parse("20161231"));
+        metric2.setAverage(100);
+        metric2.setBase(100);
+        metric2.addTag(new Tag("Location", "San Jose"));
+        metric2.addTag(new Tag("Brand", "Renting"));
+        metric2.addValue(new Value(sdf.parse("20160101"), 100));
+        metric2.addValue(new Value(sdf.parse("20160102"), 100));
+        metric2.addValue(new Value(sdf.parse("20160103"), 100));
+        
+        Level ebidta2 = new Level("Ebidta BRAC");
+        ebidta2.addLevel(metric2);
+        Level ebidta = new Level("EBITDA");
+        ebidta.addLevel(ebidta2);
+        Level financial = new Level("Financiera");
+        financial.addLevel(ebidta);
+        data.addLevel(financial);
+
+
+        Metric metric3 = new Metric("Metric", sdf.parse("20160101"), sdf.parse("20161231"));
+        metric3.setAverage(100);
+        metric3.setBase(100);
+        metric3.addTag(new Tag("Location", "San Jose"));
+        metric3.addTag(new Tag("Brand", "Renting"));
+        metric3.addValue(new Value(sdf.parse("20160101"), 100));
+        metric3.addValue(new Value(sdf.parse("20160102"), 100));
+        metric3.addValue(new Value(sdf.parse("20160103"), 100));
+        
+        Level call = new Level("Llamadas exitosas Call Center");
+        call.addLevel(metric3);
+        Level satisfaction = new Level("Satisfacción de Clientes");
+        satisfaction.addLevel(call);
+        Level client = new Level("Clientes");
+        client.addLevel(satisfaction);
+        data.addLevel(client);
+
+
+        Metric metric4 = new Metric("Metric", sdf.parse("20160101"), sdf.parse("20161231"));
+        metric4.setAverage(100);
+        metric4.setBase(100);
+        metric4.addTag(new Tag("Location", "San Jose"));
+        metric4.addTag(new Tag("Brand", "Renting"));
+        metric4.addValue(new Value(sdf.parse("20160101"), 100));
+        metric4.addValue(new Value(sdf.parse("20160102"), 100));
+        metric4.addValue(new Value(sdf.parse("20160103"), 100));
+        
+        Level execution = new Level("Cumplimiento de la induccion");
+        execution.addLevel(metric4);
+        Level promote = new Level("Promover una cultura enfocada en valores");
+        promote.addLevel(execution);
+        Level increase = new Level("Crecimiento y Aprendizaje");
+        increase.addLevel(promote);
+        data.addLevel(increase);
+        
         return new Gson().toJson(data);
     }
 }
