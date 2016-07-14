59570cdf0d70ce8f220c546d1f81232a78114ef6
Kevin Escobar Miranda <info@EscobarMiranda.com>
2016-07-14 10:04:24 -0600

    sample data hard coded into service

--- centriz-dashboard-web-service/src/main/java/cr/centriz/services/MetricService.java
+++ centriz-dashboard-web-service/src/main/java/cr/centriz/services/MetricService.java
@@ -27,23 +27,103 @@ public class MetricService {
         data.addHeader(new Header("KPI Primario"));
         data.addHeader(new Header("KPI Secundario"));
 
-        Metric metric = new Metric("Renting", sdf.parse("20160101"), sdf.parse("20161231"));
-        metric.setAverage(100);
-        metric.setBase(100);
-        metric.addTag(new Tag("Location", "San Jose"));
-        metric.addTag(new Tag("Brand", "Renting"));
-        metric.addValue(new Value(sdf.parse("20160101"), 100));
-        metric.addValue(new Value(sdf.parse("20160102"), 100));
-        metric.addValue(new Value(sdf.parse("20160103"), 100));
-        
-        Level renting = new Level("Renting");
-        renting.addLevel(metric);
-        Level inventory = new Level("Inventarios");
-        inventory.addLevel(renting);
-        Level process = new Level("Procesos");
-        process.addLevel(inventory);
-        
-        data.addLevel(process);
+        // Financiera
+        Level financiera = new Level("Financiera");
+
+        // KPI Primario
+        Level ebitda = new Level("Ebitda");
+        financiera.addLevel(ebitda);
+        // KPI Secundarios
+        Metric ebitdaBRAC = new Metric("Ebitda BRAC", sdf.parse("20160101"), sdf.parse("20161231"));
+        ebitda.addLevel(ebitdaBRAC);
+        Metric ebitdaRENT = new Metric("Ebitda RENT", sdf.parse("20160101"), sdf.parse("20161231"));
+        ebitda.addLevel(ebitdaRENT);
+
+        // KPI Primario
+        Level SF = new Level("Salud financiera");
+        financiera.addLevel(SF);
+        // KPI Secundarios
+        Metric costoFinanciero = new Metric("Costo financiero", sdf.parse("20160101"), sdf.parse("20161231"));
+        SF.addLevel(costoFinanciero);
+        Metric patrimonioNeto = new Metric("Patrimonio neto tangible", sdf.parse("20160101"), sdf.parse("20161231"));
+        SF.addLevel(patrimonioNeto);
+
+        // KPI Primario
+        Level RVV = new Level("Rendimiento de vehiculos vendidos");
+        financiera.addLevel(RVV);
+        // KPI Secundario
+        Metric precioCeroStress = new Metric("Precio cero stress", sdf.parse("20160101"), sdf.parse("20161231"));
+        RVV.addLevel(precioCeroStress);
+        Metric precioDealer = new Metric("Precio dealer", sdf.parse("20160101"), sdf.parse("20161231"));
+        RVV.addLevel(precioDealer);
+
+        // Clientes
+        Level clientes = new Level("Clientes");
+
+        // KPI Primario
+        Level satisfaccionClientes = new Level("Satisfaccion de clientes");
+        clientes.addLevel(satisfaccionClientes);
+        // KPI Secundarios
+        Metric satisfaccionGeneral = new Metric("Satisfaccion general budget", sdf.parse("20160101"), sdf.parse("20161231"));
+        satisfaccionClientes.addLevel(satisfaccionGeneral);
+        Metric satisfaccionRenting = new Metric("Satisfaccion de cliente de Renting", sdf.parse("20160101"), sdf.parse("20161231"));
+        satisfaccionClientes.addLevel(satisfaccionRenting);
+
+        // KPI Primario
+        Level CPC = new Level("Cumplir con plan de clientes");
+        clientes.addLevel(CPC);
+        // KPI Secundarios
+        Metric CPM = new Metric("Cumplimiento de presupuesto Mayorista", sdf.parse("20160101"), sdf.parse("20161231"));
+        CPC.addLevel(CPM);
+        Metric CAC = new Metric("Clientes Activos Corporativos", sdf.parse("20160101"), sdf.parse("20161231"));
+        CPC.addLevel(CAC);
+
+        // Procesos
+        Level procesos = new Level("Procesos");
+
+        // KPI Primario
+        Level IVS = new Level("Inventario de vehiculo por segmento");
+        procesos.addLevel(IVS);
+        // KPI Secundarios
+        Metric BRAC = new Metric("BRAC", sdf.parse("20160101"), sdf.parse("20161231"));
+        IVS.addLevel(BRAC);
+        Metric RENTING = new Metric("RENTING", sdf.parse("20160101"), sdf.parse("20161231"));
+        IVS.addLevel(RENTING);
+
+        // KPI Primario
+        Level RPUOperativo = new Level("RPU Operativo");
+        procesos.addLevel(RPUOperativo);
+        // KPI Secundarios
+        Metric ocupacion = new Metric("Ocupacion", sdf.parse("20160101"), sdf.parse("20161231"));
+        RPUOperativo.addLevel(ocupacion);
+        Metric TPBRAC = new Metric("Tarifa Promedio BRAC", sdf.parse("20160101"), sdf.parse("20161231"));
+        RPUOperativo.addLevel(TPBRAC);
+
+        // Crecimiento y aprendizaje
+        Level crecimientoAprendizaje = new Level("Crecimiento y aprendizaje");
+
+        // KPI Primario
+        Level KAIZEN = new Level("KAIZEN");
+        crecimientoAprendizaje.addLevel(KAIZEN);
+        // KPI Secundarios
+        Metric ISO = new Metric("Programa de ISO", sdf.parse("20160101"), sdf.parse("20161231"));
+        KAIZEN.addLevel(ISO);
+        Metric avances = new Metric("Avances de Sururaku", sdf.parse("20160101"), sdf.parse("20161231"));
+        KAIZEN.addLevel(avances);
+
+        // KPI Primario
+        Level cultura = new Level("Promover una cultura enfocada en valores");
+        crecimientoAprendizaje.addLevel(RPUOperativo);
+        // KPI Secundarios
+        Metric cumplimientoInduccion = new Metric("Cumplimiento de la induccion", sdf.parse("20160101"), sdf.parse("20161231"));
+        cultura.addLevel(cumplimientoInduccion);
+        Metric cumplimientoCultura = new Metric("Cumplimiento plan de Cultura Organizacional Anual", sdf.parse("20160101"), sdf.parse("20161231"));
+        cultura.addLevel(cumplimientoCultura);
+
+        data.addLevel(financiera);
+        data.addLevel(clientes);
+        data.addLevel(procesos);
+        data.addLevel(crecimientoAprendizaje);
 
         return new Gson().toJson(data);
     }
